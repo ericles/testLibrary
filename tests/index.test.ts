185f22e258625db49b6232588c1e6ea05e858e1a
Hmsr <hmsrenton@gmail.com>
2023-09-25 14:28:29 +1000

    comments on test file

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,9 +1,14 @@
 // index.test.ts
 import { greet } from '../index'; 
 
+// describe all tests i test suite with string
 describe('greet function', () => {
+//it defines an individual test. it takes a description string
+// and a callback function
   it('should return "Hello, World!"', () => {
     const result = greet();
+    //expect().toBe
     expect(result).toBe('Hello, World!');
+    //will pass or fail
   });
 });
