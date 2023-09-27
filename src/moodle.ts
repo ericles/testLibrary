77e8fc416846f2893fa5d15f6f53949e25528a75
Ericles S <ericles2206@live.com>
2023-09-27 15:15:54 +1000

    Add getCourseList()

--- src/moodle.ts
+++ src/moodle.ts
@@ -3,42 +3,64 @@ const axios = require('axios'); // Use require instead of import
 const API_URL = 'https://edlibrary.moodlecloud.com/webservice/rest/server.php';
 const WS_TOKEN = 'b272fd552b8ecf4292a1e0613acf2546';
 
-export function getStudentList(){
+function getStudentList(){
     
 }
 
-export function getStudentById(){
+function getStudentById(){
 
 }
 
-export function getTeacherList(){
+function getTeacherList(){
 
 }
 
-export function getTeacherById(){
+function getTeacherById(){
 
 }
 
-export function getCourseList(){
+async function getCourseList(){
+
+    try {
+        const response = await axios.get(API_URL, {
+          params: {
+            wstoken: WS_TOKEN,
+            wsfunction: 'core_course_get_courses',
+            moodlewsrestformat: 'json',
+          },
+        });
+    
+        if (response.status === 200) {
+          const data = response.data;
+          // Process the data as needed here
+          console.log('Courses:', data);
+        } else {
+          console.error('Failed to fetch data:', response.statusText);
+        }
+      } catch (error) {
+        console.error('Error:');
+      }
 
 }
 
-export function getCourseById(){
+function getCourseById(){
 
 }
 
-export function getAssignmentList(){
+function getAssignmentList(){
 
 }
 
-export function getAssignmentById(){
+function getAssignmentById(){
 
 }
 
-export function getRubricList(){
+function getRubricList(){
 
 }
 
-export function getRubricById(){
+function getRubricById(){
 
 }
+
+getCourseList()
