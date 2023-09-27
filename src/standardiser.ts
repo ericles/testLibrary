e4281e726dff2cdb98be29d12637aa92fb2a9099
jordanjr95 <107019843+jordanjr95@users.noreply.github.com>
2023-09-27 14:53:17 +1000

    Feature/canvas integration (#3)
    * Testing stuff
    
    * Adjusting pipeline
    
    * Adjusting pipeline to run on all branches
    
    * Testing pipeline
    
    * Adjusting pipeline
    
    * Work now you sob
    
    * Adding stuff to base code structure
    
    * Adjusting branch to merge with main
    
    * Adding stuff to import classes

---
+++ src/standardiser.ts
@@ -0,0 +1,19 @@
+export function standardiseStudent(){
+    
+}
+
+export function standardiseTeacher(){
+
+}
+
+export function standardiseCourse(){
+
+}
+
+export function standardiseAssignment(){
+
+}
+
+export function standardiseRubric(){
+
+}
