43516ebf9b14ff6bcdd58bcd8a74d3c4678e5323
Ericles S <ericles2206@live.com>
2023-09-27 15:11:09 +1000

    add URL

--- src/moodle.ts
+++ src/moodle.ts
@@ -1,3 +1,8 @@
+const axios = require('axios'); // Use require instead of import
+
+const API_URL = 'https://edlibrary.moodlecloud.com/webservice/rest/server.php';
+const WS_TOKEN = 'b272fd552b8ecf4292a1e0613acf2546';
+
 export function getStudentList(){
     
 }
