6703710506d6ed2ca8a4fe024bdfacd9bb9ebaad
Jordan Junior <jordan.junior@live.com>
2023-09-27 13:52:51 +1000

    Adding base code structure

---
+++ src/main.ts
@@ -0,0 +1,39 @@
+export function getStudentList(){
+
+}
+
+export function getStudentById(){
+
+}
+
+export function getTeacherList(){
+
+}
+
+export function getTeacherById(){
+
+}
+
+export function getCourseList(){
+
+}
+
+export function getCourseById(){
+
+}
+
+export function getAssignmentList(){
+
+}
+
+export function getAssignmentById(){
+
+}
+
+export function getRubricList(){
+
+}
+
+export function getRubricById(){
+
+}
