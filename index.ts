2486d936d64941176066d356300965fc337a1ff8
Jordan Junior <jordan.junior@live.com>
2023-09-27 15:22:24 +1000

    Testing canvas fetch

--- index.ts
+++ index.ts
@@ -4,7 +4,9 @@ import * as main from "./src/main";
 export function greet() {
   // console.log('Hello, World!');
   // return 'Hello, World!';
-  main.getCourseList('canvas', '7~HaO4qXhlfhquEAircCxGAE68LUIho1kmJ3JcYviNHh8j7OjJpdcmwHX6hgJAcA4n');
+  let data = main.getCourseList('canvas', '7~HaO4qXhlfhquEAircCxGAE68LUIho1kmJ3JcYviNHh8j7OjJpdcmwHX6hgJAcA4n');
+  console.log(data);
+  return data;
 }
 
 // Rest of your script
