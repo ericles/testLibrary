23c1bfc05665e11fc8e52a86febdc3d67d30b28a
Jordan Junior <jordan.junior@live.com>
2023-09-27 14:26:48 +1000

    Testing pipeline

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import * as main from "./src/main";
 
 // hello.ts
 export function greet() {
-  // console.log('Hello, World!');
+  console.log('Hello, World!');
   // return 'Hello, World!';
   return main.getStudentList();
 }
