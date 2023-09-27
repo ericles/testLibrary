7d5621c2ed6f1a0c813d72a4cb32772d8204513a
Jordan Junior <jordan.junior@live.com>
2023-09-27 14:40:52 +1000

    Adjusting branch to merge with main

--- index.ts
+++ index.ts
@@ -1,10 +1,7 @@
-import * as main from "./src/main";
-
 // hello.ts
 export function greet() {
   console.log('Hello, World!');
-  // return 'Hello, World!';
-  return main.getStudentList();
+  return 'Hello, World!';
 }
 
 // Rest of your script
--- src/canvas.ts
+++
@@ -1,3 +0,0 @@
-export function getStudentList(){
-    return "Hello, World!";
-}
