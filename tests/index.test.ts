313725e8bb4229fedfcdff298cafdbe135cb0bc0
Hamish <103914078@student.swin.edu.au>
2023-10-01 17:24:21 +1100

    change name

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,8 +1,8 @@
-import { greet } from '../index';
+import { test } from '../index';
 
 describe('greet function', () => {
   it('should return data from the API', async () => {
-    const result = await greet();
+    const result = await test();
     // console.log(result);
 
     // Expect that the result is not null or undefined
