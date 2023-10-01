7c6c6d1c811ec036d5efa4f9f8f41ec237b50a1e
Hamish <103914078@student.swin.edu.au>
2023-10-01 16:03:54 +1100

    console.log data

--- index.ts
+++ index.ts
@@ -1,13 +1,10 @@
 import * as main from "./src/main";
 
-// hello.ts
+
 export async function greet() {
-  // console.log('Hello, World!');
-  // return 'Hello, World!';
   let data = await main.getCourseList('canvas', '7~HaO4qXhlfhquEAircCxGAE68LUIho1kmJ3JcYviNHh8j7OjJpdcmwHX6hgJAcA4n');
   console.log(data);
   return data;
 }
 
-// Rest of your script
   
--- src/canvas.ts
+++ src/canvas.ts
@@ -29,9 +29,7 @@ export async function getCourseList(token: string) {
       if (!response.ok) {
         throw new Error(`Request failed with status: ${response.status}`);
       }
-  
       const data = await response.json();
-  
       return data;
     } catch (error) {
       console.error('Error fetching course list:', error);
