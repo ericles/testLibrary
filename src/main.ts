a3713ef0faf1ed411c1786004573cc3e535ba9e9
Jordan Junior <jordan.junior@live.com>
2023-10-01 17:14:17 +1100

    Adding calls to main file

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,80 @@ import * as moodle from "./moodle";
 import * as standardiser from "./standardiser";
 
 
-export function getStudentList(){
-    
+export async function getStudentList(lms: string, courseId: string, token: string){
+  let data;
+
+  switch (lms) {
+    case 'canvas':
+      try {
+        data = await canvas.getStudentList(token);
+        // console.log("getStudentListCanvas", data);
+      } catch (error) {
+        console.error("Error in getStudentListCanvas:", error);
+      }
+      break;
+    default:
+      break;
+  }
+
+  return data;
 }
 
-export function getStudentById(){
+export async function getStudentById(lms: string, courseId: string, token: string, studentId: string){
+  let data;
 
+  switch (lms) {
+    case 'canvas':
+      try {
+        data = await canvas.getStudentById(token);
+        // console.log("getStudentByIdCanvas", data);
+      } catch (error) {
+        console.error("Error in getStudentByIdCanvas:", error);
+      }
+      break;
+    default:
+      break;
+  }
+
+  return data;
 }
 
-export function getTeacherList(){
+export async function getTeacherList(lms: string, courseId: string, token: string){
+  let data;
 
+  switch (lms) {
+    case 'canvas':
+      try {
+        data = await canvas.getTeacherList(token);
+        // console.log("getTeacherListCanvas", data);
+      } catch (error) {
+        console.error("Error in getTeacherListCanvas:", error);
+      }
+      break;
+    default:
+      break;
+  }
+
+  return data;
 }
 
-export function getTeacherById(){
+export async function getTeacherById(lms: string, courseId: string, token: string, teacherId: string){
+  let data;
 
+  switch (lms) {
+    case 'canvas':
+      try {
+        data = await canvas.getTeacherById(token);
+        // console.log("getTeacherByIdCanvas", data);
+      } catch (error) {
+        console.error("Error in getTeacherByIdCanvas:", error);
+      }
+      break;
+    default:
+      break;
+  }
+
+  return data;
 }
 
 export async function getCourseList(lms: string, token: string) {
@@ -26,9 +86,9 @@ export async function getCourseList(lms: string, token: string) {
     case 'canvas':
       try {
         data = await canvas.getCourseList(token);
-        // console.log("GetCourseListCanvas", data);
+        // console.log("getCourseListCanvas", data);
       } catch (error) {
-        console.error("Error in GetCourseListCanvas:", error);
+        console.error("Error in getCourseListCanvas:", error);
       }
       break;
     default:
@@ -46,9 +106,9 @@ export async function getCourseById(lms: string, token: string, courseId: string
     case 'canvas':
       try {
         data = await canvas.getCourseById(token, courseId);
-        // console.log("GetCourseByIdCanvas", data);
+        // console.log("getCourseByIdCanvas", data);
       } catch (error) {
-        console.error("Error in GetCourseByIdCanvas:", error);
+        console.error("Error in getCourseByIdCanvas:", error);
       }
       break;
     default:
@@ -58,18 +118,78 @@ export async function getCourseById(lms: string, token: string, courseId: string
   return data;
 }
 
-export function getAssignmentList(){
+export async function getAssignmentList(lms: string, token: string, courseId: string){
+  let data;
 
+  switch (lms) {
+    case 'canvas':
+      try {
+        data = await canvas.getAssignmentList(token, courseId);
+        // console.log("getAssignmentListCanvas", data);
+      } catch (error) {
+        console.error("Error in getAssignmentListCanvas:", error);
+      }
+      break;
+    default:
+      break;
+  }
+
+  return data;
 }
 
-export function getAssignmentById(){
+export async function getAssignmentById(lms: string, token: string, courseId: string, assignmentId: string){
+  let data;
 
+  switch (lms) {
+    case 'canvas':
+      try {
+        data = await canvas.getAssignmentById(token, courseId, assignmentId);
+        // console.log("getAssignmentByIdCanvas", data);
+      } catch (error) {
+        console.error("Error in getAssignmentByIdCanvas:", error);
+      }
+      break;
+    default:
+      break;
+  }
+
+  return data;
 }
 
-export function getRubricList(){
+export async function getRubricList(lms: string, token: string, courseId: string){
+  let data;
 
+  switch (lms) {
+    case 'canvas':
+      try {
+        data = await canvas.getRubricList(token, courseId);
+        // console.log("getRubricListCanvas", data);
+      } catch (error) {
+        console.error("Error in getRubricListCanvas:", error);
+      }
+      break;
+    default:
+      break;
+  }
+
+  return data;
 }
 
-export function getRubricById(){
+export async function getRubricById(lms: string, token: string, courseId: string, rubricId: string){
+  let data;
 
+  switch (lms) {
+    case 'canvas':
+      try {
+        data = await canvas.getRubricById(token, courseId, rubricId);
+        // console.log("getRubricByIdCanvas", data);
+      } catch (error) {
+        console.error("Error in getRubricByIdCanvas:", error);
+      }
+      break;
+    default:
+      break;
+  }
+
+  return data;
 }
