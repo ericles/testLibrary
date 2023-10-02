58034de1c208c3d279364abfbf40a858527e1de6
Hamish <103914078@student.swin.edu.au>
2023-10-02 19:37:08 +1100

    actual variables

--- index.ts
+++ index.ts
@@ -19,25 +19,25 @@ export async function testCanvas() {
     const courseStudentList = await main.getStudentList('canvas', courseId, token);
     console.log("START getStudentList CANVAS" + JSON.stringify(courseStudentList) + "END");
 
-    const studentByIdData = await main.getStudentById('canvas', courseId, token, 'studentId');
+    const studentByIdData = await main.getStudentById('canvas', courseId, token, studentId);
     console.log("getStudentById CANVAS" + JSON.stringify(studentByIdData));
 
     const teacherListData = await main.getTeacherList('canvas', courseId, token);
     console.log("getTeacherList CANVAS" + JSON.stringify(teacherListData));
 
-    const teacherByIdData = await main.getTeacherById('canvas', courseId, token, 'teacherId');
+    const teacherByIdData = await main.getTeacherById('canvas', courseId, token, teacherId);
     console.log("getTeacherById CANVAS" + JSON.stringify(teacherByIdData));
 
     const assignmentListData = await main.getAssignmentList('canvas', token, courseId);
     console.log("getAssignmentList CANVAS" + JSON.stringify(assignmentListData));
 
-    const assignmentByIdData = await main.getAssignmentById('canvas', token, courseId, 'assignmentId');
+    const assignmentByIdData = await main.getAssignmentById('canvas', token, courseId, assignmentId);
     console.log("getAssignmentById CANVAS" + JSON.stringify(assignmentByIdData));
 
     const rubricListData = await main.getRubricList('canvas', token, courseId);
     console.log("getRubricList CANVAS" + JSON.stringify(rubricListData));
 
-    const rubricByIdData = await main.getRubricById('canvas', token, courseId, 'rubricId');
+    const rubricByIdData = await main.getRubricById('canvas', token, courseId, rubricId);
     console.log("getRubricById CANVAS" + JSON.stringify(rubricByIdData));
 
     return courseStudentList;
