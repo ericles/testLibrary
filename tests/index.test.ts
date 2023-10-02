98e8a93d57719c85982291022d9b2dbdcffac2b7
Hamish <103914078@student.swin.edu.au>
2023-10-03 10:37:57 +1100

    test all methods in jest

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -22,10 +22,13 @@ describe('getStudentList', () => {
   // getstudentlistcanvas
   it('should return a list of students for Canvas', async () => {
     const canvasStudentList = await main.getStudentList('canvas', canvasCourseId, canvasToken);
+    // is canvasStudentList defined?
     expect(canvasStudentList).toBeDefined();
+    // is it array?
     expect(Array.isArray(canvasStudentList)).toBe(true);
-    // specific assertions for Canvas:
+    // save first object in array to firstStudent
     const firstStudent = canvasStudentList[0];
+    // does the first student have these properties?
     expect(firstStudent).toHaveProperty('id');
     expect(firstStudent).toHaveProperty('name');
     expect(firstStudent).toHaveProperty('created_at');
@@ -39,31 +42,6 @@ describe('getStudentList', () => {
   it('should return a list of students for Moodle', async () => {
     const moodleStudentList = await main.getStudentList('moodle', moodleCourseId, moodleToken);
     expect(moodleStudentList).toBeDefined();
-    
-  });
-
-  it('should handle errors for Canvas', async () => {
-    const invalidCanvasToken = 'invalid-canvas-token';
-    const invalidCanvasCourseId = 'invalid-canvas-courseId';
-
-    try {
-      await main.getStudentList('canvas', invalidCanvasCourseId, invalidCanvasToken);
-    } catch (error) {
-      expect(error).toBeDefined();
-      
-    }
-  });
-
-  it('should handle errors for Moodle', async () => {
-    const invalidMoodleToken = 'invalid-moodle-token';
-    const invalidMoodleCourseId = 'invalid-moodle-courseId';
-
-    try {
-      await main.getStudentList('moodle', invalidMoodleCourseId, invalidMoodleToken);
-    } catch (error) {
-      expect(error).toBeDefined();
-      
-    }
   });
 });
 
@@ -71,8 +49,8 @@ describe('getStudentList', () => {
 describe('getStudentById', () => {
   it('should return a student object for Canvas', async () => {
     const canvasStudent = await main.getStudentById('canvas', canvasCourseId, canvasToken, canvasStudentId);
+    //is canvasStudent defined?
     expect(canvasStudent).toBeDefined();
-    
     // specific assertions for Canvas:
     expect(canvasStudent).toHaveProperty('id');
     expect(canvasStudent).toHaveProperty('name');
@@ -84,4 +62,127 @@ describe('getStudentById', () => {
     expect(canvasStudent).toHaveProperty('root_account');
 
   });
+
+  it('should return a student object for Moodle', async () => {
+    const moodleStudent = await main.getStudentById('moodle', moodleCourseId, moodleToken, moodleStudentId);
+    //is moodleStudent defined?
+    expect(moodleStudent).toBeDefined();
+  });
 });
+
+describe('getTeacherList', () => {
+  it('should return a list of teachers for Canvas', async () => {
+    const canvasTeacherList = await main.getTeacherList('canvas', canvasCourseId, canvasToken);
+    expect(canvasTeacherList).toBeDefined();
+    expect(Array.isArray(canvasTeacherList)).toBe(true);
+    const firstTeacher = canvasTeacherList[0];
+    expect(firstTeacher).toHaveProperty('id');
+    expect(firstTeacher).toHaveProperty('name');
+    expect(firstTeacher).toHaveProperty('created_at');
+    expect(firstTeacher).toHaveProperty('sortable_name');
+    expect(firstTeacher).toHaveProperty('short_name');
+    expect(firstTeacher).toHaveProperty('sis_user_id');
+    expect(firstTeacher).toHaveProperty('integration_id');
+    expect(firstTeacher).toHaveProperty('root_account');
+    expect(firstTeacher).toHaveProperty('login_id');
+    expect(firstTeacher).toHaveProperty('email');
+  });
+
+  it('should return a list of teachers for Moodle', async () => {
+    const moodleTeacherList = await main.getTeacherList('moodle', moodleCourseId, moodleToken);
+    expect(moodleTeacherList).toBeDefined();
+  });
+});
+
+describe('getTeacherById', () => {
+  it('should return a teacher object for Canvas', async () => {
+    const canvasTeacher = await main.getTeacherById('canvas', canvasCourseId, canvasToken, canvasTeacherId);
+    expect(canvasTeacher).toBeDefined();
+    expect(canvasTeacher).toHaveProperty('id');
+    expect(canvasTeacher).toHaveProperty('name');
+    expect(canvasTeacher).toHaveProperty('created_at');
+    expect(canvasTeacher).toHaveProperty('sortable_name');
+    expect(canvasTeacher).toHaveProperty('short_name');
+    expect(canvasTeacher).toHaveProperty('sis_user_id');
+    expect(canvasTeacher).toHaveProperty('integration_id');
+    expect(canvasTeacher).toHaveProperty('root_account');
+    expect(canvasTeacher).toHaveProperty('login_id');
+    expect(canvasTeacher).toHaveProperty('email');
+  });
+
+  it('should return a teacher object for Moodle', async () => {
+    const moodleTeacher = await main.getTeacherById('moodle', moodleCourseId, moodleToken, moodleTeacherId);
+    expect(moodleTeacher).toBeDefined();
+  });
+});
+
+describe('getAssignmentList', () => {
+  it('should return a list of assignments for Canvas', async () => {
+    const canvasAssignmentList = await main.getAssignmentList('canvas', canvasCourseId, canvasToken);
+    expect(canvasAssignmentList).toBeDefined();
+    expect(Array.isArray(canvasAssignmentList)).toBe(true);
+    const firstAssignment = canvasAssignmentList[0];
+    expect(firstAssignment).toHaveProperty('id');
+    expect(firstAssignment).toHaveProperty('description');
+    expect(firstAssignment).toHaveProperty('due_at');
+    expect(firstAssignment).toHaveProperty('points_possible');
+  });
+
+  it('should return a list of assignments for Moodle', async () => {
+    const moodleAssignmentList = await main.getAssignmentList('moodle', moodleCourseId, moodleToken);
+    expect(moodleAssignmentList).toBeDefined();
+  });
+});
+
+describe('getAssignmentById', () => {
+  it('should return an assignment object for Canvas', async () => {
+    const canvasAssignment = await main.getAssignmentById('canvas', canvasCourseId, canvasToken, canvasAssignmentId);
+    expect(canvasAssignment).toBeDefined();
+    expect(canvasAssignment).toHaveProperty('id');
+    expect(canvasAssignment).toHaveProperty('description');
+    expect(canvasAssignment).toHaveProperty('due_at');
+    expect(canvasAssignment).toHaveProperty('points_possible');
+  });
+
+  it('should return an assignment object for Moodle', async () => {
+    const moodleAssignment = await  main.getAssignmentById('moodle', moodleCourseId, moodleToken, moodleAssignmentId);
+    expect(moodleAssignment).toBeDefined();
+  });
+});
+
+describe('getRubricList', () => {
+  it('should return a list of rubrics for Canvas', async () => {
+    const canvasRubricList = await main.getRubricList('canvas', canvasCourseId, canvasToken);
+    expect(canvasRubricList).toBeDefined();
+    expect(Array.isArray(canvasRubricList)).toBe(true);
+    const firstRubric = canvasRubricList[0];
+    expect(firstRubric).toHaveProperty('id');
+    expect(firstRubric).toHaveProperty('context_id');
+    expect(firstRubric).toHaveProperty('context_type');
+    expect(firstRubric).toHaveProperty('points_possible');
+    expect(firstRubric).toHaveProperty('title');
+  });
+
+  it('should return a list of rubrics for Moodle', async () => {
+    const moodleRubricList = await main.getRubricList('moodle', moodleCourseId, moodleToken);
+    expect(moodleRubricList).toBeDefined();
+  });
+});
+
+describe('getRubricById', () => {
+  it('should return a rubric object for Canvas', async () => {
+    const canvasRubric = await main.getRubricById('canvas', canvasCourseId, canvasToken, canvasRubricId);
+    expect(canvasRubric).toBeDefined();
+    expect(canvasRubric).toHaveProperty('id');
+    expect(canvasRubric).toHaveProperty('context_id');
+    expect(canvasRubric).toHaveProperty('context_type');
+    expect(canvasRubric).toHaveProperty('points_possible');
+    expect(canvasRubric).toHaveProperty('title');
+  });
+
+  it('should return a rubric object for Moodle', async () => {
+    const moodleRubric = await main.getRubricById('moodle', moodleCourseId, moodleToken, moodleRubricId);
+    expect(moodleRubric).toBeDefined();
+  });
+});
+
