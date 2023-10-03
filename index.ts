bd11eef86e9e2d16b0aae793a0927fe4ce1c75e6
noe233 <79946562+noe233@users.noreply.github.com>
2023-10-03 11:46:40 +1100

    Revert "Feature/canvas integration"

--- index.ts
+++ index.ts
@@ -1,47 +1,51 @@
-// import * as main from "./src/main";
+import * as main from "./src/main";
+const token= '7~HaO4qXhlfhquEAircCxGAE68LUIho1kmJ3JcYviNHh8j7OjJpdcmwHX6hgJAcA4n';
+const courseId = "7809929";
+const studentId = "109412431";
+const teacherId = "109412162";
+const assignmentId = "40873657";
+const rubricId = "1743469";
+// consts to test file
+
+export async function testCanvas() {
+  try {
+    // Call and log the results of Canvas-related methods
 
 
+    //separate to different async functions in test file and get rid of testcanvas
+    const courseListData = await main.getCourseList('canvas', token);
+    console.log("getCourseList CANVAS" + JSON.stringify(courseListData));
 
+    const courseIdData = await main.getCourseById('canvas', token, courseId);
+    console.log("getCourseById CANVAS" + JSON.stringify(courseIdData));
 
-// export async function testCanvas() {
-//   try {
-//     // Call and log the results of Canvas-related methods
+    const courseStudentList = await main.getStudentList('canvas', courseId, token);
+    console.log("getStudentList CANVAS" + JSON.stringify(courseStudentList));
 
+    const studentByIdData = await main.getStudentById('canvas', courseId, token, studentId);
+    console.log("getStudentById CANVAS" + JSON.stringify(studentByIdData));
 
-//     //separate to different async functions in test file and get rid of testcanvas
-//     const courseListData = await main.getCourseList('canvas', token);
-//     console.log("getCourseList CANVAS" + JSON.stringify(courseListData));
+    const teacherListData = await main.getTeacherList('canvas', courseId, token);
+    console.log("getTeacherList CANVAS" + JSON.stringify(teacherListData));
 
-//     const courseIdData = await main.getCourseById('canvas', token, courseId);
-//     console.log("getCourseById CANVAS" + JSON.stringify(courseIdData));
+    const teacherByIdData = await main.getTeacherById('canvas', courseId, token, teacherId);
+    console.log("getTeacherById CANVAS" + JSON.stringify(teacherByIdData));
 
-//     const courseStudentList = await main.getStudentList('canvas', courseId, token);
-//     console.log("getStudentList CANVAS" + JSON.stringify(courseStudentList));
+    const assignmentListData = await main.getAssignmentList('canvas', token, courseId);
+    console.log("getAssignmentList CANVAS" + JSON.stringify(assignmentListData));
 
-//     const studentByIdData = await main.getStudentById('canvas', courseId, token, studentId);
-//     console.log("getStudentById CANVAS" + JSON.stringify(studentByIdData));
+    const assignmentByIdData = await main.getAssignmentById('canvas', token, courseId, assignmentId);
+    console.log("getAssignmentById CANVAS" + JSON.stringify(assignmentByIdData));
 
-//     const teacherListData = await main.getTeacherList('canvas', courseId, token);
-//     console.log("getTeacherList CANVAS" + JSON.stringify(teacherListData));
+    const rubricListData = await main.getRubricList('canvas', token, courseId);
+    console.log("getRubricList CANVAS" + JSON.stringify(rubricListData));
 
-//     const teacherByIdData = await main.getTeacherById('canvas', courseId, token, teacherId);
-//     console.log("getTeacherById CANVAS" + JSON.stringify(teacherByIdData));
+    const rubricByIdData = await main.getRubricById('canvas', token, courseId, rubricId);
+    console.log("getRubricById CANVAS" + JSON.stringify(rubricByIdData));
 
-//     const assignmentListData = await main.getAssignmentList('canvas', token, courseId);
-//     console.log("getAssignmentList CANVAS" + JSON.stringify(assignmentListData));
-
-//     const assignmentByIdData = await main.getAssignmentById('canvas', token, courseId, assignmentId);
-//     console.log("getAssignmentById CANVAS" + JSON.stringify(assignmentByIdData));
-
-//     const rubricListData = await main.getRubricList('canvas', token, courseId);
-//     console.log("getRubricList CANVAS" + JSON.stringify(rubricListData));
-
-//     const rubricByIdData = await main.getRubricById('canvas', token, courseId, rubricId);
-//     console.log("getRubricById CANVAS" + JSON.stringify(rubricByIdData));
-
-//     return courseStudentList;
-//   } catch (error) {
-//     console.error("Error in testCanvas:", error);
-//     throw error;
-//   }
-// }
+    return courseStudentList;
+  } catch (error) {
+    console.error("Error in testCanvas:", error);
+    throw error;
+  }
+}
