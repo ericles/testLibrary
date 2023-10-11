434ed4434c0c40567412930dfaf28c500a4cd76c
VrajPatel11 <patelvraj1132000@gmail.com>
2023-10-11 12:52:42 +1100

    Updated Teacher and Student List

--- src/moodle.ts
+++ src/moodle.ts
@@ -16,14 +16,13 @@ export async function getStudentList(courseId: string) {
     if (response.status === 200) {
       const data = response.data;
       let studentList: Array<any> = [];
-      data.forEach(function (element: any) {
+      data?.forEach(function (element: any) {
         const roles = element["roles"];
         if (roles.length > 0 && roles[0]["shortname"] == "student") {
           studentList.push(element);
         }
       });
       // Process the data as needed here
-      console.log('Courses:', studentList);
       return studentList;
     } else {
       console.error('Failed to fetch data:', response.statusText);
@@ -44,33 +43,95 @@ export async function getStudentById(courseId: string, studentId: string) {
         courseid: courseId
       },
     });
+    let studentData = null;
+
     if (response.status === 200) {
       const data = response.data;
-      let studentList: Array<any> = [];
-      data.forEach(function (element: any) {
-        if (element["id"] == studentId) {
-          studentList.push(element);
-          console.log('StudentById:', studentList);
 
-          return studentList;
+      data?.forEach(function (element: any) {
+        if (element["id"] == studentId) {
+          studentData = element;
+          return studentData;
         }
       });
     } else {
       console.error('Failed to fetch data:', response.statusText);
     }
+    return studentData;
+
   } catch (error) {
     console.log('Error:', error);
+    return null;
   }
-  return null;
 }
 
 
-export function getTeacherList() {
+export async function getTeacherList(courseId: string) {
+  try {
+    const response = await axios.get(API_URL, {
+      params: {
+        wstoken: WS_TOKEN,
+        wsfunction: 'core_enrol_get_enrolled_users',
+        moodlewsrestformat: 'json',
+        courseid: courseId
+      },
+    });
+    let teacherList: Array<any> = [];
 
+    if (response.status === 200) {
+      const data = response.data;
+      if (Array.isArray(data)) {
+        data.forEach(function (element: any) {
+          const roles = element["roles"];
+          if (roles.length > 0 && roles[0]["shortname"] == "teacher" && "editingteacher") {
+            teacherList.push(element);
+          }
+          else {
+            console.log('Failed to fetch data:', response.statusText);
+          }
+        });
+      }
+      // Process the data as needed here
+      // console.log('TeacherList:', teacherList);
+    } else {
+      console.error('Failed to fetch data:', response.statusText);
+    }
+    return teacherList;
+
+  } catch (error) {
+    console.log('Error:', error);
+    return null;
+  }
 }
 
-export function getTeacherById() {
+export async function getTeacherById(courseId: string, teacherId: string) {
+  try {
+    const response = await axios.get(API_URL, {
+      params: {
+        wstoken: WS_TOKEN,
+        wsfunction: 'core_enrol_get_enrolled_users',
+        moodlewsrestformat: 'json',
+        courseid: courseId
+      },
+    });
+    let teacherData = null;
 
+    if (response.status === 200) {
+      const data = response.data;
+      data?.forEach(function (element: any) {
+        if (element["id"] == teacherId) {
+          teacherData = element;
+          return teacherData;
+        }
+      });
+    } else {
+      console.error('Failed to fetch data:', response.statusText);
+    }
+    return teacherData;
+  } catch (error) {
+    console.log('Error:', error);
+    return null;
+  }
 }
 
 export async function getCourseList() {
