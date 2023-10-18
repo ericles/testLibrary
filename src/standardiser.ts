45f51ec90081e0716b1d0e831f4adab5670ac6d3
Ericles S <ericles2206@live.com>
2023-10-18 15:12:48 +1100

    test8

--- src/standardiser.ts
+++ src/standardiser.ts
@@ -20,35 +20,39 @@ export function standardiseStudent(lms: string, data: Record<string, any>): Reco
     };
   
     const keyMapping = lms === 'canvas' ? canvasKeyMapping : moodleKeyMapping;
-    
-      const replaceKeys = (obj: Record<string, any>): Record<string, any> => {
+  
+    console.log("keyMapping =======", keyMapping);
+    const replaceKeys = (obj: Record<string, any>): Record<string, any> => {
       const newData: Record<string, any> = {};
   
       for (const key in obj) {
-        const newKey = keyMapping[key]; // Use the mapped key if available, otherwise skip the field
-        console.log("keyMapping[key] =======", keyMapping[key]);  
-        console.log("newKey =======", newKey);  
-
-        if (newKey) {
-          if (Array.isArray(obj[key])) {
-            // If the value is an array, recursively process each element
-            newData[newKey] = obj[key].map((item: any) => replaceKeys(item));
-          } else if (typeof obj[key] === 'object' && obj[key] !== null) {
-            // If the value is an object, recursively process it
-            newData[newKey] = replaceKeys(obj[key]);
-          } else {
-            newData[newKey] = obj[key];
-          }
+        const newKey = keyMapping[key]; // Use the mapped key if available, otherwise keep the key as is
+  
+        if (Array.isArray(obj[key])) {
+          // If the value is an array, recursively process each element
+          newData[newKey] = obj[key].map((item: any) => replaceKeys(item));
+        } else if (typeof obj[key] === 'object' && obj[key] !== null) {
+          // If the value is an object, recursively process it
+          newData[newKey] = replaceKeys(obj[key]);
+        } else {
+          newData[newKey] = obj[key];
         }
       }
-
-      console.log("newData =======", newData);  
+      
+      console.log("newData =======", newData);
       return newData;
     };
-  
+
     const standardizedData = replaceKeys(data);
-    console.log("standardizedData =======", standardizedData);  
-    return standardizedData;
+    const result: Record<string, any> = {};
+    for (const key in standardizedData) {
+        if (key !== keyMapping[key]) {
+          result[key] = standardizedData[key];
+        }
+      }
+      console.log("result =======", result);
+      return result;
+    /* return replaceKeys(data); */
   }
 
 
