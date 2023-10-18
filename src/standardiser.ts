a8357e233816e5f29f1245eea9311e7125e7440a
Ericles S <ericles2206@live.com>
2023-10-18 23:38:12 +1100

    test24

--- src/standardiser.ts
+++ src/standardiser.ts
@@ -40,6 +40,7 @@ function replaceKeys(obj: any, keyMapping: Record<string, string>): any {
     for (const key in obj) {
       const newKey = keyMapping[key]; // Use the mapped key if available, otherwise keep the key as is
 
+      console.log('Chave ====', newData[newKey], obj[key]);
       newData[newKey] = obj[key] !== undefined ? obj[key] : null;
     }
 
